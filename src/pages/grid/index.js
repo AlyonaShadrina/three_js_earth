a858d988a7aa005a8bb8654bbcf16b5a9a4c5b6a
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-21 10:23:30 +0200

    [ref] line creation

--- src/pages/grid/index.js
+++ src/pages/grid/index.js
@@ -9,6 +9,7 @@ import ThreeSceneBuilder from '../../ThreeSceneBuilder/ThreeSceneBuilder';
 import "../../style.css";
 import "../../navigation";
 import car from '../../assets/tesla_cybertruck/out.glb';
+import { addLine } from './grid';
 
 
 
@@ -39,7 +40,6 @@ gridB.initRenderer({
      },
      rotation: {
          x: -.1,
-         // y: Math.PI / 2,
      }
     })
     .initLight({
@@ -51,72 +51,8 @@ gridB.initRenderer({
             y: 700,
             z: -350,
         },
-        // rotation: {
-        //     // x: Math.PI / 4,
-        // },
     });
 
-// console.log('gridB', gridB);
-
-// gridB.light.lookAt(gridB.scene.position);
-
-const division = 50;
-const limit = 2000;
-const grid = new THREE.GridHelper(limit * 4, division, "purple", "purple");
-
-const moveable = [];
-for (let i = 0; i <= division; i++) {
-    moveable.push(1, 1, 0, 0); // move horizontal lines only (1 - point is moveable)
-}
-grid.geometry.addAttribute(
-    "moveable",
-    new THREE.BufferAttribute(new Uint8Array(moveable), 1)
-);
-
-grid.material = new THREE.ShaderMaterial({
-    uniforms: {
-        time: {
-            value: 0
-        },
-        limits: {
-            value: new THREE.Vector2(-limit, limit)
-        },
-        speed: {
-            value: 5
-        }
-    },
-    vertexShader: `
-    uniform float time;
-    uniform vec2 limits;
-    uniform float speed;
-
-    attribute float moveable;
-
-    varying vec3 vColor;
-
-    void main() {
-      vColor = color;
-      float limLen = limits.y - limits.x;
-      vec3 pos = position;
-      if (floor(moveable + 0.5) > 0.5){ // if a point has "moveable" attribute = 1
-        float dist = speed * time;
-        float currPos = mod((pos.z + dist) - limits.x, limLen) + limits.x;
-        pos.z = currPos;
-      }
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos,1.0);
-    }
-  `,
-    fragmentShader: `
-    varying vec3 vColor;
-
-    void main() {
-      gl_FragColor = vec4(vColor, 1.);
-    }
-  `,
-    vertexColors: THREE.VertexColors,
-});
-
-// gridB.scene.add(grid);
 
 const clock = new THREE.Clock();
 let time = 0;
@@ -127,27 +63,14 @@ let time = 0;
 var loader = new GLTFLoader();
 
 loader.load(car, function (gltf) {
-    // console.log('gltf.scene', gltf.scene);
     gltf.scene.position.y = 120;
     gltf.scene.rotation.y = Math.PI;
     gltf.scene.position.z = 320;
     gridB.scene.add(gltf.scene);
     renderB();
-    console.log('gltf.scene', gltf.scene);
-
-    // const light = new THREE.AmbientLight(0xFFFFFF, 1);
-    // const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-    // light.position.z = -1000;
-    // light.position.y = 1000;
-
-
-    // gltf.scene.add(light);
-
 }, undefined, function (error) {
     console.error( error );
 });
-
-
 
 const controls = new TrackballControls(gridB.camera, gridB.renderer.domElement);
 controls.rotateSpeed = 1.0;
@@ -164,12 +87,6 @@ var bloomPass = new UnrealBloomPass(
     1,
     .2
 );
-// bloomPass.threshold = .4;
-// bloomPass.strength = 2.7;
-// bloomPass.radius = .1;
-
-
-// gridB.renderer.toneMappingExposure = Math.pow( 1, 4);
 
 const composer = new EffectComposer( gridB.renderer );
 composer.addPass( renderScene );
@@ -180,43 +97,26 @@ const size = 500;
 const count = 20;
 const cell = 50;
 
-var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3( - size, 0, 0 ) );
-geometry.vertices.push(new THREE.Vector3( size, 0, 0 ) );
 
-const linesMaterial = new THREE.LineBasicMaterial( { color: 0x787878, opacity: .2, linewidth: 2 } );
-
-function addLine({ name, position, rotation }) {
-    gridB.createLine({
-        name: name,
-        geometry,
-        position,
-        material: {
-            type: 'Basic',
-            props: {
-                color: 'magenta',
-                linewidth: 2
-            },
-        },
-        rotation
-    })
-}
-
-for ( var i = 0; i <= count; i++ ) {
+for ( let i = 0; i <= count; i++ ) {
     addLine({
+        builder: gridB,
         name: `linex-${i}`,
         position: {
             z: ( i * cell ) - size,
         },
+        size,
     });
     addLine({
+        builder: gridB,
         name: `liney-${i}`,
         position: {
             x: ( i * cell ) - size,
         },
         rotation: {
             y: 90 * Math.PI / 180
-        }
+        },
+        size,
     });
 }
 
@@ -226,7 +126,6 @@ function renderB() {
     requestAnimationFrame(renderB);
     // time += clock.getDelta();
     time += .3;
-    grid.material.uniforms.time.value = time;
 
     Object.keys(gridB.lines).map(name => {
         if (name.includes('x')) {
@@ -237,10 +136,12 @@ function renderB() {
                 const selectedObject = gridB.scene.getObjectByName(name);
                 gridB.scene.remove(selectedObject);
                 addLine({
+                    builder: gridB,
                     name:  `linex-${Date.now()}`,
                     position: {
                         z: ( 0 * cell ) - size,
                     },
+                    size,
                 });
             }
         }
