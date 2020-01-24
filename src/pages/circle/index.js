c1f17b807b9fc7e406e81c639e525df7abd7fd39
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-24 17:37:21 +0200

    [ref] drag events to class

--- src/pages/circle/index.js
+++ src/pages/circle/index.js
@@ -32,94 +32,84 @@ basic.addEventListener({
     listener: mouseListener,
 });
 
-const controls = new TrackballControls(basic.camera, basic.renderer.domElement);
-controls.rotateSpeed = 1.0;
-controls.zoomSpeed = 1.2;
-controls.panSpeed = 0.8;
-controls.keys = [65, 83, 68];
+// const controls = new TrackballControls(basic.camera, basic.renderer.domElement);
+// controls.rotateSpeed = 1.0;
+// controls.zoomSpeed = 1.2;
+// controls.panSpeed = 0.8;
+// controls.keys = [65, 83, 68];
 
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
+class MouseInteractions {
+    constructor() {
+        this.INTERSECTED = null;
+        this.SELECTED = null;
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+    }
 
-let INTERSECTED;
-let SELECTED;
+    onMouseMove( event ) {
+
+        // this.mouse = new THREE.Vector2();
+
+
+        event.preventDefault();
+        this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        // find intersections
+        this.raycaster.setFromCamera( this.mouse, basic.camera );
+        var intersects = this.raycaster.intersectObjects( basic.scene.children );
+        if ( intersects.length > 0 ) {
+            if ( this.INTERSECTED != intersects[ 0 ].object ) {
+                if ( this.INTERSECTED && this.INTERSECTED.material.emissive) this.INTERSECTED.material.emissive.setHex( this.INTERSECTED.currentHex );
+                this.INTERSECTED = intersects[ 0 ].object;
+                this.INTERSECTED.currentHex = this.INTERSECTED.material.emissive.getHex();
+                this.INTERSECTED.material.emissive.setHex( 0xff0000 );
+                // console.log(INTERSECTED.position);
+            }
+        } else {
+            if ( this.INTERSECTED && this.INTERSECTED.material.emissive) this.INTERSECTED.material.emissive.setHex( this.INTERSECTED.currentHex );
+            this.INTERSECTED = null;
+        }
+
+        if (this.SELECTED && intersects[0]) {
+            const centerPositionRadius = intersects[0].point.x ** 2 + intersects[0].point.y ** 2;
+
+            if (centerPositionRadius <= (radius - this.SELECTED.geometry.parameters.radius) ** 2) {
+                this.SELECTED.position.copy(intersects[0].point);
+            }
+        }
+
+    }
+
+    onMouseDown() {
+
+        var intersects = this.raycaster.intersectObjects( basic.scene.children );
+        if (intersects[0]) {
+            this.SELECTED = intersects[0].object
+        }
+    }
+
+    onMouseUp() {
+        this.SELECTED = null
+    }
+
+    addInteractions(builder) {
+        builder.renderer.domElement.addEventListener( 'mousedown', (e) => this.onMouseDown(e), false );
+        builder.renderer.domElement.addEventListener( 'mouseup', (e) => this.onMouseUp(e), false );
+        builder.renderer.domElement.addEventListener( 'mousemove', (e) => this.onMouseMove(e), false );
+    }
+}
+
+const interact = new MouseInteractions();
+interact.addInteractions(basic);
 
 render();
 
-function onMouseMove( event ) {
-
-    event.preventDefault();
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-    // find intersections
-    raycaster.setFromCamera( mouse, basic.camera );
-    var intersects = raycaster.intersectObjects( basic.scene.children );
-    if ( intersects.length > 0 ) {
-        if ( INTERSECTED != intersects[ 0 ].object ) {
-            if ( INTERSECTED && INTERSECTED.material.emissive) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-            INTERSECTED = intersects[ 0 ].object;
-            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            INTERSECTED.material.emissive.setHex( 0xff0000 );
-            // console.log(INTERSECTED.position);
-
-            const coordX = mouse.x;
-            const coordY = mouse.y;
-
-            const vec = {
-                x: (coordX - INTERSECTED.position.x),
-                y: (-coordY - INTERSECTED.position.y),
-            };
-
-
-
-
-            const circleRadius = INTERSECTED.geometry.parameters.radius
-
-            const speed = 5000 * circleRadius;
-
-            const centerPositionRadius = (INTERSECTED.position.x + (vec.x * 1 / speed)) ** 2 + (INTERSECTED.position.y + (vec.y * 1 / speed)) ** 2;
-
-
-            if (centerPositionRadius <= (radius - circleRadius) ** 2) {
-                // INTERSECTED.position.x = coordX;
-                // INTERSECTED.position.y = coordY;
-            }
-
-        }
-    } else {
-        if ( INTERSECTED && INTERSECTED.material.emissive) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-        INTERSECTED = null;
-    }
-
-    if (SELECTED && intersects[0]) {
-        const centerPositionRadius = intersects[0].point.x ** 2 + intersects[0].point.y ** 2;
-
-        if (centerPositionRadius <= (radius - SELECTED.geometry.parameters.radius) ** 2) {
-            SELECTED.position.copy(intersects[0].point);
-        }
-    }
-
-}
-
-
-basic.renderer.domElement.addEventListener( 'mousedown', function(event) {
-    var intersects = raycaster.intersectObjects( basic.scene.children );
-    if (intersects[0]) {
-        SELECTED = intersects[0].object
-    }
-}, false );
-
-basic.renderer.domElement.addEventListener( 'mouseup', function(event) {
-    SELECTED = null
-}, false );
-
-
-basic.renderer.domElement.addEventListener( 'mousemove', onMouseMove, false );
 
 function render() {
-    raycaster.setFromCamera( mouse, basic.camera );
+    interact.raycaster.setFromCamera( interact.mouse, basic.camera );
     requestAnimationFrame(render);
 
     basic.update();
     // controls.update();
 }
+
