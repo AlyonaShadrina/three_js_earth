1560761cdb0880bdc77f44d77992f09f781be52a
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-27 10:57:10 +0200

    Updates

--- sw.js
+++ sw.js
@@ -12,12 +12,12 @@
 console.log('self', self);
 self.addEventListener('install', (event) => {
     console.log('install', event);
-
+    console.log('--1');
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/three_js_earth/earth-small.html',
-                '/three_js_earth/earth-small.js',
+                './three_js_earth/earth-small.html',
+                './three_js_earth/earth-small.js',
             ]);
         })
     );
