a3805ac2206a71f14e09e7be5411039468ff3021
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-27 13:01:11 +0200

    [add] sw works offline for earth page

--- dist/sw.js
+++ dist/sw.js
@@ -9,48 +9,48 @@
 // //     console.log(`Boo! Workbox didn't load 😬`);
 // // }
 // //
-const version = 'v4'
+// const version = 'v4'
 
 console.log('self', self);
+
+const APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+const VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
+const CACHE_NAME = APP_PREFIX + VERSION
+const URLS = [                            // Add URL you want to cache in this list.
+    '/three_js_earth/',                     // If you have separate JS/CSS files,
+    '/three_js_earth/earth-small.html',            // add path to those files here
+    '/three_js_earth/earthSmall.js',            // add path to those files here
+]
+
 self.addEventListener('install', (event) => {
     console.log('install', event);
-    console.log('version', version);
+    console.log('CACHE_NAME', CACHE_NAME);
     event.waitUntil(
-        caches.open(version).then((cache) => {
-            return cache.addAll([
-                '/three_js_earth/earth-small.html',
-                '/three_js_earth/earthSmall.js',
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(URLS);
         })
     );
 });
-// var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-// var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-// var CACHE_NAME = APP_PREFIX + VERSION
-// var URLS = [                            // Add URL you want to cache in this list.
-//     '/three_js_earth/',                     // If you have separate JS/CSS files,
-//     '/three_js_earth/earth-small.html'            // add path to those files here
-//     '/three_js_earth/earthSmall.js'            // add path to those files here
-// ]
+
 //
-// // Respond with cached resources
-// self.addEventListener('fetch', function (e) {
-//     console.log('fetch request : ' + e.request.url)
-//     e.respondWith(
-//         caches.match(e.request).then(function (request) {
-//             if (request) { // if cache is available, respond with cache
-//                 console.log('responding with cache : ' + e.request.url)
-//                 return request
-//             } else {       // if there are no cache, try fetching request
-//                 console.log('file is not cached, fetching : ' + e.request.url)
-//                 return fetch(e.request)
-//             }
-//
-//             // You can omit if/else for console.log & put one line below like this too.
-//             // return request || fetch(e.request)
-//         })
-//     )
-// })
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    )
+})
 //
 // // Cache resources
 // self.addEventListener('install', function (e) {
@@ -62,24 +62,25 @@ self.addEventListener('install', (event) => {
 //     )
 // })
 //
-// // Delete outdated caches
-// self.addEventListener('activate', function (e) {
-//     e.waitUntil(
-//         caches.keys().then(function (keyList) {
-//             // `keyList` contains all cache names under your username.github.io
-//             // filter out ones that has this app prefix to create white list
-//             var cacheWhitelist = keyList.filter(function (key) {
-//                 return key.indexOf(APP_PREFIX)
-//             })
-//             // add current cache name to white list
-//             cacheWhitelist.push(CACHE_NAME)
-//
-//             return Promise.all(keyList.map(function (key, i) {
-//                 if (cacheWhitelist.indexOf(key) === -1) {
-//                     console.log('deleting cache : ' + keyList[i] )
-//                     return caches.delete(keyList[i])
-//                 }
-//             }))
-//         })
-//     )
-// })
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    console.log('activate', e);
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // `keyList` contains all cache names under your username.github.io
+            // filter out ones that has this app prefix to create white list
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            // add current cache name to white list
+            cacheWhitelist.push(CACHE_NAME)
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] )
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
