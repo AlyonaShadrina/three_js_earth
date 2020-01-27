7f4588acc8bbf07f6e1ed4a54974b995d31b5a14
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-27 11:47:27 +0200

    Updates

--- sw.js
+++ sw.js
@@ -14,12 +14,12 @@
 console.log('self', self);
 
 const APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-const VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+const VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
 const CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [                            // Add URL you want to cache in this list.
     '/three_js_earth/',                     // If you have separate JS/CSS files,
     '/three_js_earth/earth-small.html',            // add path to those files here
-    '/three_js_earth/earthSmall.js'            // add path to those files here
+    '/three_js_earth/earthSmall.js',            // add path to those files here
 ]
 
 self.addEventListener('install', (event) => {
@@ -62,24 +62,25 @@ self.addEventListener('install', (event) => {
 //     )
 // })
 //
-// // Delete outdated caches
-// self.addEventListener('activate', function (e) {
-//     e.waitUntil(
-//         caches.keys().then(function (keyList) {
-//             // `keyList` contains all cache names under your username.github.io
-//             // filter out ones that has this app prefix to create white list
-//             var cacheWhitelist = keyList.filter(function (key) {
-//                 return key.indexOf(APP_PREFIX)
-//             })
-//             // add current cache name to white list
-//             cacheWhitelist.push(CACHE_NAME)
-//
-//             return Promise.all(keyList.map(function (key, i) {
-//                 if (cacheWhitelist.indexOf(key) === -1) {
-//                     console.log('deleting cache : ' + keyList[i] )
-//                     return caches.delete(keyList[i])
-//                 }
-//             }))
-//         })
-//     )
-// })
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    console.log('activate', e);
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // `keyList` contains all cache names under your username.github.io
+            // filter out ones that has this app prefix to create white list
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            // add current cache name to white list
+            cacheWhitelist.push(CACHE_NAME)
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] )
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
